8aba389092cfe673bc5eb7569a5392b978a9d573
Oleksii Vasin <104771167+vasin-oleksii@users.noreply.github.com>
2025-05-15 10:43:37 +0200

    add model User

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log("juggernaut");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.dbUrl);
+    console.log(process.env.dbUrl);
+    app.listen(PORT, () => {
+      console.log(`Server on ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
+
+const apiUrl = process.env.REACT_APP_API_URL;
